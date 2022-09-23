2c27ff55cb7a431cd10a3795c78066c53131228c
VladikPC <vladislavtimirbaev@gmail.com>
2022-09-23 23:25:29 +0500

    Make search

---
+++ StartPage/static/js/script.js
@@ -0,0 +1,10 @@
+let searchText = document.getElementById("search-text")
+let searchButton = document.getElementById("search-button")
+
+let urlExample = "https://yandex.ru/search/?text="
+
+function search() {
+	if (searchText.value != "") {
+    open(urlExample + searchText.value)
+	}
+}
